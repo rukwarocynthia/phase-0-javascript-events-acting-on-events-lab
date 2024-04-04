b6c6c07a20d5ab165bd9f1e269d000096b427407
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-04-05 00:17:30 +0300

    acting

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    dodger.style.left = parseInt(dodger.style.left) - 1 + 'px';
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    dodger.style.left = parseInt(dodger.style.left) + 1 + 'px';
+  }
+  
